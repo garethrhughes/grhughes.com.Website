fb6395e970ec797171de9512a3737ce366463d4c
Gareth Hughes <gareth@grhughes.com>
2013-01-24 17:22:19 +0000

    Front end wiring up and pagination

---
+++ src/grhughes.com.Website.Core/Model/BlogViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace grhughes.com.Website.Core.Model
+{
+  using System.Collections.Generic;
+
+  public class BlogViewModel
+  {
+    public int Page { get; set; }
+    public int TotalPosts { get; set; }
+    public int Limit { get; set; }
+    public IList<BlogPost> Posts { get; set; }
+  }
+}
