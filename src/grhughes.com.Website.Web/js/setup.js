b83efc67a7ee7084a90632c97730e7fc1562a01c
Gareth Hughes <gareth@grhughes.com>
2013-08-02 22:36:00 +1000

    asdf

--- src/grhughes.com.Website.Web/js/setup.js
+++ src/grhughes.com.Website.Web/js/setup.js
@@ -1,6 +1,39 @@
 ﻿(function () {
 
+  var sidebar = $('#sidebar'),
+      top = 148,
+      height = sidebar.height(),
+      footer = $('footer'),
+      mainContent = $('#main-content'),
+      leftContainer = $('div.span3');
+
+  var checkScroll = function () {
+    if (sidebar.height() >= mainContent.height()) return;
+    if (sidebar.height() >= ($(window).height() - 25)) {
+      sidebar.css({ position: 'relative', top: '0', width: 'auto', height: 'auto' });
+      return;
+    }
+
+    var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    if (scrollTop > (top - 20)) {
+      var height = sidebar.height(),
+          footerTop = footer.offset().top;
+
+      if (scrollTop + height > (footerTop - 40)) {
+        sidebar.css({ position: 'fixed', top: -(scrollTop + height - footerTop + 20), width: leftContainer.width(), height: height });
+      } else {
+        sidebar.css({ position: 'fixed', top: '20px', width: leftContainer.width(), height: height });
+      }
+    } else {
+      sidebar.css({ position: 'relative', top: '0', width: 'auto', height: 'auto' });
+    }
+  };
+
   $().ready(function () {
+    $(window).scroll(checkScroll);
+    $(window).resize(checkScroll);
+    checkScroll();
+
     $('#slideshow').embedagram({
       instagram_id: '12485765',
       limit: 12
