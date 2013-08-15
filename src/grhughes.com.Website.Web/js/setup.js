acf484a53f7125ebbaf4cf7a69bd05839bbad931
Gareth Hughes <gareth@grhughes.com>
2013-08-15 18:07:27 +1000

    new header

--- src/grhughes.com.Website.Web/js/setup.js
+++ src/grhughes.com.Website.Web/js/setup.js
@@ -1,7 +1,7 @@
 ﻿(function () {
 
   var sidebar = $('#sidebar'),
-      top = 428,
+      top = 378,
       height = sidebar.height(),
       footer = $('footer'),
       mainContent = $('#main-content'),
